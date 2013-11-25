af4a8d5540c9fc051d3f775c696ddb92f873c784
Sciyned <zeshadx@hotmail.com>
2013-11-24 22:02:42 -0800

    Initial commit

---
+++ MainForm.cs
@@ -0,0 +1,52 @@
+﻿/*  Program:        
+    Author:         Mike Chiu
+    Class:          CISP41
+    Date:           
+    Description: 
+
+    I certify that the code below is my own work.
+ 
+ Exception(s): N/A
+
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Project4
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class PayrollMainForm : Form
+	{
+		public PayrollMainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		void ExitButtonClick(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+		
+		void SalaryRadioButtonCheckedChanged(object sender, EventArgs e)
+		{
+			hoursTextField.Enabled = false;
+		}
+		
+		void HourlyRadioButtonCheckedChanged(object sender, EventArgs e)
+		{
+			hoursTextField.Enabled = true;
+		}
+	}
+}
---
+++ SplashForm.Designer.cs
@@ -0,0 +1,128 @@
+﻿/*  Program:        
+    Author:         Mike Chiu
+    Class:          CISP41
+    Date:           
+    Description: 
+
+    I certify that the code below is my own work.
+ 
+ Exception(s): N/A
+
+*/
+
+namespace Project4
+{
+	partial class SplashForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(66, 230);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(154, 23);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Loading Payroll Application:";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// label2
+			// 
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(12, 19);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(260, 40);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Payroll Application";
+			this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// label3
+			// 
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(80, 76);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(129, 23);
+			this.label3.TabIndex = 2;
+			this.label3.Text = "Programmers";
+			this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// label4
+			// 
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.Location = new System.Drawing.Point(90, 112);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(107, 23);
+			this.label4.TabIndex = 3;
+			this.label4.Text = "Oscar Campos";
+			this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// label5
+			// 
+			this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label5.Location = new System.Drawing.Point(90, 135);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(100, 23);
+			this.label5.TabIndex = 4;
+			this.label5.Text = "Mike Chiu";
+			this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// timer1
+			// 
+			this.timer1.Enabled = true;
+			this.timer1.Interval = 5000;
+			// 
+			// SplashForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 262);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "SplashForm";
+			this.Text = "SplashForm";
+			this.ResumeLayout(false);
+		}
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+	}
+}
