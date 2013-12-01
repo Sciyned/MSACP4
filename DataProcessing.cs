194fbab4f160020a39104d26e609bac71b0aab38
someone5544 <mistareal58@gmail.com>
2013-12-01 07:29:24 -0800

    Added Employee class and basic file reading

--- DataProcessing.cs
+++ DataProcessing.cs
@@ -11,23 +11,86 @@
 */
 
 using System;
+using System.IO;
 
 namespace Project4
 {
-	/// <summary>
-	/// The purpose of this class is to process the data. This class will retrieve data when the form
-	/// loads from the .dat files. This class will also be used to save/overwrite the .dat files should
-	/// there be things that have been changed. 
-	/// 
-	/// For the most part, try and take the information from Accounts.cs
-	/// </summary>
-	public class DataProcessing
-	{
-		public DataProcessing()
-		{
-			
-		}
-		
-		
-	}
+    /// <summary>
+    /// The purpose of this class is to process the data. This class will retrieve data when the form
+    /// loads from the .dat files. This class will also be used to save/overwrite the .dat files should
+    /// there be things that have been changed. 
+    /// 
+    /// For the most part, try and take the information from Accounts.cs
+    /// </summary>
+    public class DataProcessing
+    {
+        private string dataFileString;
+        private StreamReader dataReader;
+        private StreamWriter dataWriter;
+
+        public DataProcessing()
+        {
+            dataFileString = "";
+        }
+
+        public DataProcessing(string file, bool readFile)
+        {
+            dataFileString = file;
+            if (readFile)
+            {
+                // Open file for reading
+                dataReader = new StreamReader(dataFileString);
+            }
+            else
+            {
+                // Open file for writing
+                dataWriter = new StreamWriter(dataFileString);
+            }
+        }
+
+        public bool hasLine()
+        {
+            if (dataReader != null)
+            {
+                // Reader object was created
+                if (dataReader.Peek() != -1)
+                {
+                    // There are lines available to read
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                // Reader object not created
+                return false;
+            }
+        }
+
+        // Returns the read line as an array. Line is split every ' ' (space) character.
+        public string[] getLine()
+        {
+            if (dataReader == null)
+            {
+                // Create StreamReader object
+                dataReader = new StreamReader(dataFileString);
+            }
+
+            if (hasLine())
+            {
+                // Lines available to read
+                string[] dataPieces = dataReader.ReadLine().Split(' ');
+
+                return dataPieces;
+            }
+            else
+            {
+                // Return empty string when no more lines are available
+                return new string[0];
+            }
+        }
+    }
 }
--- MainForm.cs
+++ MainForm.cs
@@ -24,6 +24,11 @@ namespace Project4
 	{
 		// Instantiate objects from other classes
 		EditEmployee showForm = new EditEmployee();
+
+        // Variables and array to hold employee information
+        const int MAX_EMPLOYEES = 50;
+        Employee[] employeeArray = new Employee[MAX_EMPLOYEES];
+        int employeeCountInt = 0;
 		
 		public PayrollMainForm()
 		{
@@ -54,10 +59,89 @@ namespace Project4
 		
 		void PayrollMainFormLoad(object sender, EventArgs e)
 		{
-			DataProcessing LoadData = new DataProcessing();
+			//DataProcessing LoadData = new DataProcessing();
+            // Read data from file
+            DataProcessing myData = new DataProcessing("data/master.dat", true);
+
+            while (myData.hasLine())
+            {
+                string[] linePieceArray = myData.getLine();
+                int pieceInt = 0;
+                employeeArray[employeeCountInt] = new Employee();
+
+                foreach (string pieceString in linePieceArray)
+                {
+                    if (pieceString != "")
+                    {
+                        // Piece isn't blank, read information into array
+
+                        if (pieceInt == 0)
+                        {
+                            // Employee ID
+                            employeeArray[employeeCountInt].ID = int.Parse(pieceString);
+                        }
+                        else if (pieceInt == 1)
+                        {
+                            // First Name
+                            employeeArray[employeeCountInt].FirstName = pieceString;
+                        }
+                        else if (pieceInt == 2)
+                        {
+                            // Last Name
+                            employeeArray[employeeCountInt].LastName = pieceString;
+                        }
+                        else if (pieceInt == 3)
+                        {
+                            // Department
+                            employeeArray[employeeCountInt].Department = pieceString;
+                        }
+                        else if (pieceInt == 4)
+                        {
+                            // Position
+                            employeeArray[employeeCountInt].Position = pieceString;
+                        }
+                        else if (pieceInt == 5)
+                        {
+                            // Salary
+                            employeeArray[employeeCountInt].Salary = double.Parse(pieceString);
+                        }
+                        else if (pieceInt == 6)
+                        {
+                            // Medical/Dental
+                            employeeArray[employeeCountInt].Insurance = double.Parse(pieceString);
+                        }
+                        else if (pieceInt == 7)
+                        {
+                            // Life Insurance
+                            employeeArray[employeeCountInt].LifeInsurance = double.Parse(pieceString);
+                        }
+                        else if (pieceInt == 8)
+                        {
+                            // 401k
+                            employeeArray[employeeCountInt].Retirement = double.Parse(pieceString);
+                        }
+                        else if (pieceInt == 9)
+                        {
+                            // FSA
+                            employeeArray[employeeCountInt].FSA = double.Parse(pieceString);
+                        }
+                        pieceInt++;
+                    }
+                }
+                employeeCountInt++;
+                Console.WriteLine(employeeCountInt);
+            }
+
+            // Insert results into form
+            for (int i = 0; i < employeeCountInt; i++)
+            {
+                employeeIDComboBox.Items.Add(employeeArray[i].ID);
+                employeeFirstNameComboBox.Items.Add(employeeArray[i].FirstName);
+                employeeLastNameComboBox.Items.Add(employeeArray[i].LastName);
+            }
 		}
-		
-		void EditEmployeeToolStripMenuItemClick(object sender, EventArgs e)
+
+        void EditEmployeeToolStripMenuItemClick(object sender, EventArgs e)
 		{
 			showForm.ShowDialog();
 		}
